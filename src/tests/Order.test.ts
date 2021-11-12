2c1127e823bd57419de17be2324acef233ad16c7
pedroalles <pedroalles@gmail.com>
2021-11-12 18:22:18 -0300

    Add Order test

---
+++ src/tests/Order.test.ts
@@ -0,0 +1,13 @@
+test("Deve criar um pedido", function () {
+
+    //given
+    const order = new Order();
+    order.addItem(new Item('Beer', 'Brahma', 10));
+    order.addItem(new Item('Water', 'Fonte Ijuí', 2));
+
+    //when
+    const total = order.getTotal();
+
+    //then
+    expect(total).toBe(12);
+});
