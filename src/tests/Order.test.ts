8933c86452ac50d20c91be8110e59b66789169fb
pedroalles <pedroalles@gmail.com>
2021-11-12 19:39:03 -0300

    Add Order taxes test

--- src/tests/Order.test.ts
+++ src/tests/Order.test.ts
@@ -1,16 +1,20 @@
 import Item from '../Item';
 import Order from '../Order';
 
-test("Deve criar um pedido", function () {
-
-    //given
+test('Deve criar um pedido e calcular o total', function () {
     const order = new Order();
     order.addItem(new Item('Beer', 'Brahma', 10));
+    order.addItem(new Item('Whisky', ' Jack Daniels', 100));
     order.addItem(new Item('Water', 'Fonte Ijuí', 2));
-
-    //when
     const total = order.getTotal();
+    expect(total).toBe(112);
+});
 
-    //then
-    expect(total).toBe(12);
+test('Deve criar um pedido e calcular os impostos', function () {
+    const order = new Order();
+    order.addItem(new Item('Beer', 'Brahma', 10));
+    order.addItem(new Item('Whisky', ' Jack Daniels', 100));
+    order.addItem(new Item('Water', 'Fonte Ijuí', 2));
+    const taxes = order.getTaxes();
+    expect(taxes).toBe(21);
 });
